df57ef61df3fd24c7662c2aeeda998b572c0b526
Brandon Brown <brandonbrown@Brandons-iMac.local>
2016-11-05 12:00:55 -0400

    update with tokens and basic functionality

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const fbtoken = process.env.FB_PAGE_ACCESS_TOKEN
+const vtoken = process.env.FB_VERIFY_ACCESS_TOKEN
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
@@ -20,7 +23,7 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'makingItWorkOnHeroku') {
+    if (req.query['hub.verify_token'] === vtoken) {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
@@ -30,3 +33,35 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:fbtoken},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
